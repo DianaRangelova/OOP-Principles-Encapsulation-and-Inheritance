1db69964014b9e02aaf77dbf5ae205a3373e7d90
DianaRangelova <diana_kirilova@mail.bg>
2024-04-04 23:05:22 +0300

    OOP Principles Encapsulation and Inheritance in C#

---
+++ Players/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Players
+{
+    internal class Program
+    {
+        public static void Main() 
+        {
+            Hero hero = new Hero("diana", 300);
+            Elf elf = new Elf("petko", 100);
+            BladeKnight bladeKnight = new BladeKnight ("pesho", 100);
+
+            Console.WriteLine(hero);
+            Console.WriteLine(elf);
+            Console.WriteLine(bladeKnight);
+        }
+    }
+}
